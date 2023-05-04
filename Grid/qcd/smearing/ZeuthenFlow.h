7e6f5c57f20ff680b10a0c82e5c4bcd52ba22933
dbollweg <bollwegdennis@gmail.com>
2023-05-03 20:10:30 -0400

    added adaptive stepsize for zeuthen flow

--- Grid/qcd/smearing/ZeuthenFlow.h
+++ Grid/qcd/smearing/ZeuthenFlow.h
@@ -33,6 +33,8 @@ public:
         assert(0);
     }
 
+    void zeuthen_force(typename Gimpl::GaugeField &U, typename Gimpl::GaugeField &Z) const;
+
     static RealD energyDensityPlaquette(const RealD t, const GaugeField& U);
 
     std::vector<RealD> flowMeasureEnergyDensityPlaquette(GaugeField& V, const GaugeField& U, int measure_interval = 1);
@@ -50,7 +52,7 @@ private:
 
     void evolve_step(typename Gimpl::GaugeField &U, RealD &tau) const;
 
-    void zeuthen_force(typename Gimpl::GaugeField &U, typename Gimpl::GaugeField &Z) const;
+    // void zeuthen_force(typename Gimpl::GaugeField &U, typename Gimpl::GaugeField &Z) const;
 
 public:
     INHERIT_GIMPL_TYPES(Gimpl)
@@ -95,7 +97,7 @@ void ZeuthenFlowBase<Gimpl>::setDefaultMeasurements(int topq_meas_interval){
 }
 
 template<class Gimpl>
-void ZeuthenFlow<Gimpl>::zeuthen_force(typename Gimpl::GaugeField &U, typename Gimpl::GaugeField &Z) const{
+void ZeuthenFlowBase<Gimpl>::zeuthen_force(typename Gimpl::GaugeField &U, typename Gimpl::GaugeField &Z) const{
     GaugeField Zprime(U.Grid());
     for (int mu = 0; mu < Nd; mu++) {
         GaugeLinkField Ulink(U.Grid());
@@ -119,14 +121,14 @@ void ZeuthenFlow<Gimpl>::evolve_step(typename Gimpl::GaugeField &U, RealD &tau) 
     //in contrast to Wilson flow, we now need to apply the cov. derivatives
     //from the zeuthen flow equation!
 
-    zeuthen_force(U, Z);
+    this->zeuthen_force(U, Z);
 
     Z*= 0.25;
     Gimpl::update_field(Z, U, -2.0*epsilon);
 
     Z *= -17.0/8.0;
     this->SG.deriv(U, tmp);
-    zeuthen_force(U, tmp);
+    this->zeuthen_force(U, tmp);
     Z += tmp;
 
     Z *= 8.0/9.0;
@@ -134,7 +136,7 @@ void ZeuthenFlow<Gimpl>::evolve_step(typename Gimpl::GaugeField &U, RealD &tau) 
 
     Z *= -4.0/3.0;
     this->SG.deriv(U, tmp);
-    zeuthen_force(U,tmp);
+    this->zeuthen_force(U,tmp);
     Z += tmp;
 
     Z *= 3.0/4.0;
@@ -170,6 +172,110 @@ void ZeuthenFlow<Gimpl>::smear(GaugeField& out, const GaugeField& in) const{
       if( step % meas.first == 0 ) meas.second(step,taus,out);
   }
 }
+
+
+template<class Gimpl>
+class ZeuthenFlowAdaptive: public ZeuthenFlowBase<Gimpl>{
+private:
+    RealD init_epsilon;
+    RealD maxTau;
+    RealD tolerance;
+
+    int evolve_step_adaptive(typename Gimpl::GaugeField& U, RealD& tau, RealD &eps) const;
+public:
+    INHERIT_GIMPL_TYPES(Gimpl)
+
+    ZeuthenFlowAdaptive(const RealD init_epsilon, const RealD maxTau, const RealD tolerance, unsigned int meas_interval = 1): ZeuthenFlowBase<Gimpl>(meas_interval), init_epsilon(init_epsilon), maxTau(maxTau), tolerance(tolerance) {}
+
+    void smear(GaugeField& out, const GaugeField& in) const override;
+};
+
+
+template<class Gimpl>
+int ZeuthenFlowAdaptive<Gimpl>::evolve_step_adaptive(typename Gimpl::GaugeField& U, RealD& tau, RealD & eps) const {
+    
+    if (maxTau - tau < eps){
+        eps = maxTau-tau;
+    }
+
+    GaugeField Z(U.Grid());
+    GaugeField Zprime(U.Grid());
+    GaugeField tmp(U.Grid()), Uprime(U.Grid()), Usave(U.Grid());
+    Uprime = U;
+    Usave = U;
+
+    this->SG.deriv(U,Z);
+    this->zeuthen_force(U,Z);
+
+    Zprime = -Z;
+    Z *= 0.25;
+    Gimpl::update_field(Z,U,-2.0*eps);
+
+    Z *= -17.0/8.0;
+    this->SG.deriv(U,tmp);
+    this->zeuthen_force(U,tmp);
+    Z += tmp;
+    Zprime += 2.0*tmp;
+    Z *= 8.0/9.0;
+    Gimpl::update_field(Z, U, -2.0*eps);
+
+    Z *= -4.0/3.0;
+    this->SG.deriv(U, tmp);
+    this->zeuthen_force(U,tmp);
+    Z += tmp;
+
+    Z *= 3.0/4.0;
+    Gimpl::update_field(Z,U, -2.0*eps);
+
+    Gimpl::update_field(Zprime,Uprime,-2.0*eps);
+
+    GaugeField diffU = U - Uprime;
+    RealD max_dist = 0;
+
+    for (int mu = 0; mu < Nd; mu++) {
+        typename Gimpl::GaugeLinkField diffU_mu = PeekIndex<LorentzIndex>(diffU, mu);
+        RealD dist_mu = sqrt(maxLocalNorm2(diffU_mu))/Nc/Nc;
+        max_dist = std::max(max_dist,dist_mu);
+    }
+
+    int ret;
+    if(max_dist < tolerance) {
+        tau += eps;
+        ret = 1;
+    }
+    else {
+        U = Usave;
+        ret = 0;
+    }
+
+    eps = eps*0.95*std::pow(tolerance/max_dist,1./3.);
+    std::cout << GridLogMessage << "Adaptive smearing : Distance : " << max_dist << " Step successful: " << ret << "New epsilon: " << eps << std::endl;
+
+    return ret;
+}
+
+template <class Gimpl>
+void ZeuthenFlowAdaptive<Gimpl>::smear(GaugeField& out, const GaugeField& in) const {
+    std::cout << GridLogMessage
+	    << "[ZeuthenFlow] initial epsilon : " << init_epsilon << std::endl;
+  std::cout << GridLogMessage
+	    << "[ZeuthenFlow] full trajectory : " << maxTau << std::endl;
+  std::cout << GridLogMessage
+	    << "[ZeuthenFlow] tolerance   : " << tolerance << std::endl;
+  out = in;
+  RealD taus = 0.;
+  RealD eps = init_epsilon;
+  unsigned int step = 0;
+  do{
+    int step_success = evolve_step_adaptive(out, taus, eps); 
+    step += step_success; 
+
+    if(step_success)
+      for(auto const &meas : this->functions)
+	if( step % meas.first == 0 ) meas.second(step,taus,out);
+  } while (taus < maxTau);
+}
+
 
         
 NAMESPACE_END(Grid);
---
+++ tests/smearing/Test_ZeuthenFlowAdaptive.cc
@@ -0,0 +1,101 @@
+#include <Grid/Grid.h>
+
+namespace Grid{
+  struct ZFParameters: Serializable {
+    GRID_SERIALIZABLE_CLASS_MEMBERS(ZFParameters,
+            int, steps,
+            double, step_size,
+            int, meas_interval,
+            double, maxTau); // for the adaptive algorithm
+
+    ZFParameters() {}       
+
+    template <class ReaderClass >
+    ZFParameters(Reader<ReaderClass>& Reader){
+      read(Reader, "ZeuthenFlow", *this);
+    }
+
+  };
+
+  struct ConfParameters: Serializable {
+    GRID_SERIALIZABLE_CLASS_MEMBERS(ConfParameters,
+           std::string, conf_prefix,
+            std::string, rng_prefix,
+				    int, StartConfiguration,
+				    int, EndConfiguration,
+            int, Skip);
+  
+    template <class ReaderClass >
+    ConfParameters(Reader<ReaderClass>& Reader){
+      read(Reader, "Configurations", *this);
+    }
+
+  };
+}
+
+int main(int argc, char **argv) {
+  using namespace Grid;
+   ;
+
+  Grid_init(&argc, &argv);
+  GridLogLayout();
+
+  auto latt_size   = GridDefaultLatt();
+  auto simd_layout = GridDefaultSimd(Nd, vComplex::Nsimd());
+  auto mpi_layout  = GridDefaultMpi();
+  GridCartesian               Grid(latt_size, simd_layout, mpi_layout);
+  GridRedBlackCartesian     RBGrid(&Grid);
+
+//  std::vector<int> seeds({1, 2, 3, 4, 5});
+  GridSerialRNG sRNG;
+  GridParallelRNG pRNG(&Grid);
+  //pRNG.SeedFixedIntegers(seeds);
+
+  LatticeGaugeField Umu(&Grid), Uflow(&Grid);
+  //SU<Nc>::HotConfiguration(pRNG, Umu);
+  
+  typedef Grid::XmlReader       Serialiser;
+  Serialiser Reader("input_zf.xml");
+  ZFParameters ZFPar(Reader);
+  ConfParameters CPar(Reader);
+  CheckpointerParameters CPPar(CPar.conf_prefix, CPar.rng_prefix);
+  NerscHmcCheckpointer<PeriodicGimplR> CPBin(CPPar);
+
+  for (int conf = CPar.StartConfiguration; conf <= CPar.EndConfiguration; conf+= CPar.Skip){
+
+  CPBin.CheckpointRestore(conf, Umu, sRNG, pRNG);
+
+  std::cout << std::setprecision(15);
+  std::cout << GridLogMessage << "Initial plaquette: "
+    << WilsonLoops<PeriodicGimplR>::avgPlaquette(Umu) << std::endl;
+
+  int t=ZFPar.maxTau;
+  ZeuthenFlowAdaptive<PeriodicGimplR> ZF(ZFPar.step_size, ZFPar.maxTau,1.0e-4,
+					ZFPar.meas_interval);
+
+  ZF.smear(Uflow, Umu);
+
+  RealD WFlow_plaq = WilsonLoops<PeriodicGimplR>::avgPlaquette(Uflow);
+  RealD WFlow_TC   = WilsonLoops<PeriodicGimplR>::TopologicalCharge(Uflow);
+  RealD WFlow_T0   = ZF.energyDensityPlaquette(t,Uflow);
+  std::cout << GridLogMessage << "Plaquette          "<< conf << "   " << WFlow_plaq << std::endl;
+  std::cout << GridLogMessage << "T0                 "<< conf << "   " << WFlow_T0 << std::endl;
+  std::cout << GridLogMessage << "TopologicalCharge  "<< conf << "   " << WFlow_TC   << std::endl;
+
+  std::cout<< GridLogMessage << " Admissibility check:\n";
+  const double sp_adm = 0.067;                // admissible threshold
+  const double pl_adm = 1.0-sp_adm/Nc;
+  std::cout << GridLogMessage << "   (pl_adm =" << pl_adm << ")\n";
+
+  // Need min and reduce min for this function
+  //double sp_max = NC_*(1.0-stpl.plaq_min(U,pl_adm));
+  double sp_ave = Nc*(1.0-WFlow_plaq);
+
+  //std::cout<< GridLogMessage << "   sp_max = "        << sp_max <<"\n";
+  std::cout<< GridLogMessage << "   sp_ave = "        << sp_ave <<"\n";
+  std::cout<< GridLogMessage << "   (sp_admissible = "<< sp_adm <<")\n";
+  //std::cout<< GridLogMessage << "   sp_admissible - sp_max = "<<sp_adm-sp_max <<"\n";
+  std::cout<< GridLogMessage << "   sp_admissible - sp_ave = "<<sp_adm-sp_ave <<"\n";
+  }
+  Grid_finalize();
+}  // main
