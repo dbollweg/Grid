f5ce9d0d4aea767ff75c15e67e4019b2b3080adf
dbollweg <bollwegdennis@gmail.com>
2023-05-12 21:25:41 -0400

    Fixed error in application of last laplacian in FermionFlow.h, Test_FermionZeuthenFlow.cc now contains proper comparison to fft solution

--- Grid/qcd/smearing/FermionFlow.h
+++ Grid/qcd/smearing/FermionFlow.h
@@ -8,8 +8,8 @@ private:
     int Nstep;
     RealD epsilon;
 
-    void evolve_step(typename Gimpl::GaugeField &U, SpinorField chi, RealD &tau) const;
-    void fermion_laplacian(typename Gimpl::GaugeField &U, SpinorField chi_in, SpinorField chi_out) const;
+    void evolve_step(typename Gimpl::GaugeField &U, SpinorField &chi, RealD &tau) const;
+    void fermion_laplacian(typename Gimpl::GaugeField &U, SpinorField &chi_in, SpinorField &chi_out) const;
 
 public:
     INHERIT_GIMPL_TYPES(Gimpl)
@@ -21,31 +21,32 @@ public:
 };
 
 template<class Gimpl, class SpinorField>
-void FermionFlow<Gimpl, SpinorField>::fermion_laplacian(typename Gimpl::GaugeField &U, SpinorField chi_in, SpinorField chi_out) const {
+void FermionFlow<Gimpl, SpinorField>::fermion_laplacian(typename Gimpl::GaugeField &U, SpinorField &chi_in, SpinorField &chi_out) const {
     chi_out = 0;
     for (int mu = 0; mu < Nd; mu++) {
         GaugeLinkField Ulink(U.Grid());
         Ulink = PeekIndex<LorentzIndex>(U,mu);
         chi_out += Ulink*Cshift(chi_in,mu,1)-2.0*chi_in+adj(Gimpl::CshiftLink(Ulink,mu,-1))*Cshift(chi_in,mu,-1);
     }
+    return;
 }
 
 template<class Gimpl, class SpinorField>
-void FermionFlow<Gimpl,SpinorField>::evolve_step(typename Gimpl::GaugeField &U, SpinorField chi, RealD &tau) const {
+void FermionFlow<Gimpl,SpinorField>::evolve_step(typename Gimpl::GaugeField &U, SpinorField &chi, RealD &tau) const {
     GaugeField Z(U.Grid());
     GaugeField tmp(U.Grid());
     SpinorField phi0(U.Grid());
     SpinorField phi1(U.Grid());
     SpinorField phi2(U.Grid());
     //W0
-    phi0=chi;
+    
 
     this->SG.deriv(U,Z);
     this->zeuthen_force(U,Z);
     fermion_laplacian(U,chi,phi0);
     Z*=0.25;
     Gimpl::update_field(Z, U, -2.0*epsilon);
-    phi1 = chi + 0.25*epsilon*phi0;
+    phi1 = chi + 0.25*epsilon*phi0; //phi1 = phi0 + 1/4eps*laplace(W0)phi0
     //U is now W1
 
     Z *= -17.0/8.0;
@@ -53,9 +54,10 @@ void FermionFlow<Gimpl,SpinorField>::evolve_step(typename Gimpl::GaugeField &U, 
     this->zeuthen_force(U, tmp);
     
 
-    fermion_laplacian(U,phi1,phi2); //phi2 is laplace(W1)phi1
+    fermion_laplacian(U,phi1,phi2); //phi2 is laplace(W1)phi1   
     //phi2:
-    phi2 = chi + 8.0/9.0*epsilon*phi2-2.0/9.0*phi0;
+    phi2 = chi + 8.0/9.0*epsilon*phi2-2.0/9.0*epsilon*phi0; //phi2 = phi0 + 8/9*eps*laplace(W1)phi1-2/9*epsilon*laplace(W0)phi0
+    // std::cout << GridLogMessage << "Testing phi0 " << norm2(PeekIndex<SpinorIndex>(phi0,0)) << std::endl;
     Z += tmp;
 
     Z *= 8.0/9.0;
@@ -69,15 +71,17 @@ void FermionFlow<Gimpl,SpinorField>::evolve_step(typename Gimpl::GaugeField &U, 
 
     Z *= 3.0/4.0;
 
-    fermion_laplacian(U,phi2,phi2);
-    chi = phi1 + 3.0/4.0*epsilon*phi2;
+    fermion_laplacian(U,phi2,phi0);
+    
+    chi = phi1 + 3.0/4.0*epsilon*phi0; ///phi3 = phi + 3/4*eps*laplace(W2)phi2
+    
 
     Gimpl::update_field(Z, U, -2.0*epsilon);
 
 
     tau+=epsilon;
 
-
+    return;
 
 
 }
@@ -91,7 +95,7 @@ void FermionFlow<Gimpl,SpinorField>::smear(GaugeField& out, SpinorField& chi_out
     std::cout << GridLogMessage
         << "[FermionFlow + ZeuthenFlow] full trajectory : " << Nstep * epsilon << std::endl;
 
-
+    SpinorField tmp(chi_out.Grid());
     out = in;
     chi_out = chi_in;
 
@@ -99,6 +103,8 @@ void FermionFlow<Gimpl,SpinorField>::smear(GaugeField& out, SpinorField& chi_out
     for (unsigned int step = 1; step <= Nstep; step++) { //step indicates the number of smearing steps applied at the time of measurement
     auto start = std::chrono::high_resolution_clock::now();
     evolve_step(out, chi_out, taus);
+    tmp=chi_out;
+    std::cout << GridLogMessage << "[FermionFlow + ZeuthenFLow]  chi norm after step = " << norm2(PeekIndex<SpinorIndex>(tmp,0)) << std::endl;
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end - start;
 #ifdef WF_TIMING
--- tests/smearing/Test_FermionZeuthenFlow.cc
+++ tests/smearing/Test_FermionZeuthenFlow.cc
@@ -30,11 +30,56 @@ namespace Grid{
     }
 
   };
+
+
+// void fft_solve(GridCartesian& thisgrid, const LatticeFermionD& src, LatticeFermionD &sol, RealD t) {
+//   FFT theFFT(&thisgrid);
+//   LatticeFermionD tmp(&thisgrid);
+//   theFFT.FFT_all_dim(tmp,src,FFT::forward); //tmp = U(omega,0)
+
+//   Coordinate latt_size = thisgrid.FullDimensions();
+
+//   LatticeComplexD C(&thisgrid); //this is going to be omega^2
+//   LatticeComplexD coor(&thisgrid);
+//   C=Zero();
+
+//   for(int mu =0; mu<4; mu++) {
+//     RealD TwoPiL = M_PI * 2.0 / latt_size[mu];
+//     LatticeCoordinate(coor,mu);
+//     C = C + (TwoPiL*coor)*(TwoPiL*coor); // C = (2pi*n/L)^2 (omega)^2
+//   }  
+//   tmp = exp(-1.0*C*t) * tmp; //tmp = exp(-omega^2*t)U(omega,0) 
+
+//   theFFT.FFT_all_dim(sol,tmp,FFT::backward);
+
+// }
+
+void dfft_solve(GridCartesian& thisgrid, const LatticeFermionD& src, LatticeFermionD &sol, RealD epsilon) {
+  FFT theFFT(&thisgrid);
+  LatticeFermionD tmp(&thisgrid);
+  theFFT.FFT_all_dim(tmp,src,FFT::forward); //tmp = U(omega,0)
+
+  Coordinate latt_size = thisgrid.FullDimensions();
+
+  LatticeComplexD C(&thisgrid);
+  LatticeComplexD coor(&thisgrid);
+
+  C=Zero();
+  for (int mu = 0; mu < Nd; mu++) {
+    RealD PiL = M_PI/latt_size[mu];
+    LatticeCoordinate(coor,mu);
+    C = C + sin(PiL*coor)*sin(PiL*coor);
+  }
+
+  tmp = tmp - 4*epsilon*C*tmp; 
+  theFFT.FFT_all_dim(sol,tmp,FFT::backward);
+}
+
 }
 
 int main(int argc, char **argv) {
+  
   using namespace Grid;
-   ;
 
   Grid_init(&argc, &argv);
   GridLogLayout();
@@ -63,6 +108,7 @@ int main(int argc, char **argv) {
   for (int conf = CPar.StartConfiguration; conf <= CPar.EndConfiguration; conf+= CPar.Skip){
 
   CPBin.CheckpointRestore(conf, Umu, sRNG, pRNG);
+  SU<Nc>::ColdConfiguration(pRNG,Umu);
 
   std::cout << std::setprecision(15);
   std::cout << GridLogMessage << "Initial plaquette: "
@@ -70,17 +116,26 @@ int main(int argc, char **argv) {
   
   LatticeFermionD src(&Grid);
   LatticeFermionD phi(&Grid);
+  // LatticeFermionD phi_fft(&Grid);
+  LatticeFermionD phi_dfft(&Grid);
 
   random(pRNG, src);
+  // fft_solve(Grid,src,phi_fft,20*ZFPar.step_size);
+  LatticeFermionD tmp_src(&Grid);
+  tmp_src=src;
+  for (int i = 0; i < 200; i++) {
+    dfft_solve(Grid, tmp_src, phi_dfft, ZFPar.step_size);
+    tmp_src=phi_dfft;
+  }
   int t=ZFPar.maxTau;
-  std::cout << GridLogMessage << "Testing ZFPar stepsize " << ZFPar.step_size << std::endl;
+  // std::cout << GridLogMessage << "Testing ZFPar stepsize " << ZFPar.step_size << std::endl;
 
-  std::cout << GridLogMessage << "Testing ZFPar maxTau " << ZFPar.maxTau << std::endl;
-  FermionFlow<PeriodicGimplR,LatticeFermionD> ZF(ZFPar.step_size, 20,
+  // std::cout << GridLogMessage << "Testing ZFPar maxTau " << ZFPar.maxTau << std::endl;
+  FermionFlow<PeriodicGimplR,LatticeFermionD> ZF(ZFPar.step_size, 200,
 					ZFPar.meas_interval);
 
   ZF.smear(Uflow, phi, Umu, src);
-
+  src=phi;
   RealD WFlow_plaq = WilsonLoops<PeriodicGimplR>::avgPlaquette(Uflow);
   RealD WFlow_TC   = WilsonLoops<PeriodicGimplR>::TopologicalCharge(Uflow);
   RealD WFlow_T0   = ZF.energyDensityPlaquette(t,Uflow);
@@ -88,20 +143,16 @@ int main(int argc, char **argv) {
   std::cout << GridLogMessage << "T0                 "<< conf << "   " << WFlow_T0 << std::endl;
   std::cout << GridLogMessage << "TopologicalCharge  "<< conf << "   " << WFlow_TC   << std::endl;
 
-  std::cout<< GridLogMessage << " Admissibility check:\n";
-  const double sp_adm = 0.067;                // admissible threshold
-  const double pl_adm = 1.0-sp_adm/Nc;
-  std::cout << GridLogMessage << "   (pl_adm =" << pl_adm << ")\n";
-
-  // Need min and reduce min for this function
-  //double sp_max = NC_*(1.0-stpl.plaq_min(U,pl_adm));
-  double sp_ave = Nc*(1.0-WFlow_plaq);
-
-  //std::cout<< GridLogMessage << "   sp_max = "        << sp_max <<"\n";
-  std::cout<< GridLogMessage << "   sp_ave = "        << sp_ave <<"\n";
-  std::cout<< GridLogMessage << "   (sp_admissible = "<< sp_adm <<")\n";
-  //std::cout<< GridLogMessage << "   sp_admissible - sp_max = "<<sp_adm-sp_max <<"\n";
-  std::cout<< GridLogMessage << "   sp_admissible - sp_ave = "<<sp_adm-sp_ave <<"\n";
+  std::cout << GridLogMessage << "Norm of src = " << norm2(PeekIndex<SpinorIndex>(src,0)) << std::endl;
+  // std::cout << GridLogMessage << "Norm of phi_fft - phi_flow = " << norm2(PeekIndex<SpinorIndex>(diff,0)) << std::endl;
+  // std::cout << GridLogMessage << "Norm of phi_fft = " << norm2(PeekIndex<SpinorIndex>(phi_fft,0)) << std::endl;
+  std::cout << GridLogMessage << "Norm of phi = " << norm2(PeekIndex<SpinorIndex>(phi,0)) << std::endl;
+  std::cout << GridLogMessage << "Norm of phi_dfft = " << norm2(PeekIndex<SpinorIndex>(phi_dfft,0)) << std::endl;
+  LatticeFermionD diff = phi_dfft - phi;
+  std::cout << GridLogMessage << "Norm of phi_dfft - phi_flow = " << norm2(PeekIndex<SpinorIndex>(diff,0)) << std::endl;
+  auto avg = (norm2(PeekIndex<SpinorIndex>(diff,0))+norm2(PeekIndex<SpinorIndex>(diff,1))+norm2(PeekIndex<SpinorIndex>(diff,2))+norm2(PeekIndex<SpinorIndex>(diff,3)))/Grid.lSites()/4.0;
+  std::cout << GridLogMessage << "Norm/vol/Ns of phi_dfft - phi_flow = " << avg << std::endl;
+  assert(avg < 1e-6);
   }
   Grid_finalize();
 }  // main
